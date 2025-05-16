f096829c5f5c8077182459becabc107aae211953
king_ultron99 <lochym10@gmail.com>
2025-05-16 17:31:09 +1000

    Chore: remove unnecessary folders from vc

--- build/generated/sources/templates/com/king_ultron99/hyperChat/BuildConstants.java
+++
@@ -1,7 +0,0 @@
-package com.king_ultron99.hyperChat;
-
-// The constants are replaced before compilation
-public class BuildConstants {
-
-    public static final String VERSION = "1.0-SNAPSHOT";
-}
