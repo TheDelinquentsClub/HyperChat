6f0e9704955e5c60604135bb61ec6e950e03b8dc
BAKKBONE Australia <hosting@bakkbone.com.au>
2025-05-18 22:48:12 +0800

    Text formatting - refactor then implement combination

--- src/main/kotlin/com/king_ultron99/hyperChat/utils/TextReplacement.kt
+++ src/main/kotlin/com/king_ultron99/hyperChat/utils/TextReplacement.kt
@@ -1,11 +1,17 @@
 package com.king_ultron99.hyperChat.utils
 
 import com.google.inject.Inject
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.MiniMessage
 import net.luckperms.api.LuckPermsProvider
 import net.william278.papiproxybridge.api.PlaceholderAPI
 import java.util.UUID
 
-class Placeholders @Inject constructor() {
+class TextReplacement @Inject constructor() {
+
+    fun parseString(message: String, player: UUID): String? {
+        return parseMiniMessageString(parsePlaceholders(parseHCPlaceholders(message, player).toString(), player).toString())
+    }
     
     fun parsePlaceholders(message: String, player: UUID): String? {
         val papi = PlaceholderAPI.createInstance()
@@ -21,7 +27,16 @@ class Placeholders @Inject constructor() {
         val suffix = luckPermsUser?.cachedData?.metaData?.suffix.toString()
         val nickname = luckPermsUser?.cachedData?.metaData?.getMetaValue("nickname").toString()
 
-        return message.replace("{PREFIX}", prefix, false).replace("{SUFFIX}", suffix, false).replace("{NICKNAME}", nickname, false)
+        return message.replace("{PREFIX}", prefix, false).replace("{SUFFIX}", suffix, false)
+            .replace("{NICKNAME}", nickname, false)
     }
-    
+
+    fun parseMiniMessageString(message: String): String {
+        return MiniMessage.miniMessage().deserialize(message).toString()
+    }
+
+    fun parseMiniMessageComponent(message: String): Component {
+        return MiniMessage.miniMessage().deserialize(message)
+    }
+
 }
